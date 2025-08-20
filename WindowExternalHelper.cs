486528404a877939de3e20997023f696bc93507b
Igor Pukhlikov <47010778+igorpooh1978@users.noreply.github.com>
2025-08-20 18:03:31 +0300

    Update WindowExternalHelper.cs

--- WindowExternalHelper.cs
+++ WindowExternalHelper.cs
@@ -1,182 +1,357 @@
 using Resto.Front.Api;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Windows.Input;
-using System.Windows;
 using System;
-// To Syrve
-namespace WindowExternalHelper
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace IikoWindows;
+
+/// <summary>
+/// Класс работы со своими окнами в iiko.<br/> В .csproj обязательно добавьте<br/>&lt;UseWPF&gt;true&lt;/UseWPF&gt;
+/// </summary>
+public static class Helpers
 {
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+    [DllImport("user32.dll")]
+    private static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+    private const string iikoName = "iikoFront.Net";
+    private const string syrveName = "syrveFront.Net";
+    private const IntPtr _frontHwnd;
+
     /// <summary>
-    /// Class for Managing Windows.
+    /// Инициализация визуального окна. Делается в конце метода инициализации вызываемого класса плагина
     /// </summary>
-    public static class Helpers
+    public static void InitializeUiDispatcher()
     {
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        if (Application.Current != null)
+            return;
 
-        /// <summary>
-        /// Initializing the Dispatcher of a WPF Application.
-        /// </summary>
-        public static void InitializeUiDispatcher()
+        var runningProcesses = Process.GetProcessesByName(iikoName).SingleOrDefault();
+        if (runningProcesses == null)
         {
-            if (Application.Current != null) return;
+            runningProcesses = Process.GetProcessesByName(syrveName).SingleOrDefault();
+            if (runningProcesses == null)
+            {
+                throw new Exception("Unable to find front process. Ensure that the application is running.");
+            }
+        }
 
-            using var uiThreadStartedSignal = new ManualResetEvent(false);
-            var thread = new Thread(() =>
+        _frontHwnd = runningProcesses.MainWindowHandle;
+
+        using var uiThreadStartedSignal = new ManualResetEvent(false);
+        var thread = new Thread(() =>
+        {
+            var app = new Application { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+            StartupEventHandler startupHandler = null;
+            startupHandler = (s, e) =>
             {
-                var app = new Application { ShutdownMode = ShutdownMode.OnExplicitShutdown };
-                app.Startup += (_, _) => uiThreadStartedSignal.Set();
-                app.Run();
-            })
-            {
-                IsBackground = true
+                // ReSharper disable once AccessToDisposedClosure (вызывающий поток не покинет блок using, пока мы вызовем Set)
+                uiThreadStartedSignal.Set();
+                app.Startup -= startupHandler;
             };
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
+            app.Startup += startupHandler;
+            app.Run();
+        });
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
 
-            uiThreadStartedSignal.WaitOne();
-        }
+        uiThreadStartedSignal.WaitOne();
 
-        /// <summary>
-        /// Shutting Down the Dispatcher of a WPF Application.
-        /// </summary>
-        public static void ShutdownUiDispatcher()
+    }
+
+    /// <summary>
+    /// Метод отключения инициализации окна. Делается методе Dispose вызываемого класса плагина
+    /// </summary>
+    public static void ShutdownUiDispatcher()
+    {
+        Application.Current.Dispatcher.InvokeShutdown();
+        Application.Current.Dispatcher.Thread.Join();
+    }
+
+
+    /// <summary>
+    /// Метод отображения WPF окна. В классе окна <b>TWindow</b> необходимо создать метод <b>AddExternalProperties</b> с получаемыми аргументами <b>TArgs args</b>
+    /// </summary>
+    /// <typeparam name="TWindow">Класс WPF окна</typeparam>
+    /// <typeparam name="TRequest">Тип передаваемых аргументов</typeparam>
+    /// <typeparam name="TResult">Тип возвращаемых данных</typeparam>
+    /// <param name="args">передаваемые данные</param>
+    /// <returns>Возвращает данные <b>TResult</b></returns>
+    public static TResult ShowWindow<TWindow, TRequest, TResult>(TRequest args)
+        where TWindow : System.Windows.Window, new()
+        where TRequest : class
+        where TResult : class
+    {
+        var source = new CancellationTokenSource();
+        TWindow window = null;
+        TResult result = null;
+        PluginContext.Log.Info($"ShowWindow.{typeof(TWindow).Name} :: External screen opened.");
+
+        var t = Task.Run(() =>
         {
-            if (Application.Current == null) return;
-
-            Application.Current.Dispatcher.InvokeShutdown();
-            Application.Current.Dispatcher.Thread.Join();
-        }
-
-        /// <summary>
-        /// Displaying a WPF Window with the Ability to Pass Arguments and Receive a Result.
-        /// </summary>
-        public static TResult ShowWindow<TWindow, TRequest, TResult>(TRequest args)
-            where TWindow : Window, new()
-            where TRequest : class
-            where TResult : class
-        {
-            TResult result = null;
-            try
+            window = Application.Current.Dispatcher.Invoke(() =>
             {
-                PluginContext.Log.Info($"ShowWindow: Opening window {typeof(TWindow).Name}");
-
-                Task.Run(() =>
+                try
                 {
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        var window = new TWindow();
-                        var argsMethod = typeof(TWindow).GetMethod("AddExternalProperties");
+                    PluginContext.Log.Info($"ShowWindow.{typeof(TWindow).Name}.Invoke :: External screen opened.");
+                    window = new TWindow();
 
-                        if (argsMethod != null)
+                    var windowHandle = new WindowInteropHelper(window).EnsureHandle();
+                    // Устанавливаем родителя
+          
+
+                    var argsMethod = typeof(TWindow).GetMethod("AddExternalProperties");
+                    if (argsMethod is not null)
+                    {
+                        if (args is not null)
                         {
                             argsMethod.Invoke(window, new object[] { args });
                         }
                         else
                         {
-                            PluginContext.Log.Warn($"Method AddExternalProperties not found in {typeof(TWindow).Name}.");
+                            argsMethod.Invoke(window, null);
+                            PluginContext.Log.Warn(
+                                $"ShowWindow.{typeof(TWindow).Name} :: AddExternalProperties {nameof(TRequest)} args is null.");
                         }
+                    }
+                    else
+                    {
+                        PluginContext.Log.Warn(
+                            $"ShowWindow.{typeof(TWindow).Name} :: Doesn't have AddExternalProperties method.");
+                    }
+                    SetParent(windowHandle, _frontHwnd);
+                    EnableWindow(_frontHwnd, false);
+                    
+                    window.Closed += (s, e) =>
+                    {
+                        // Разблокируем родительское окно
+                        EnableWindow(_frontHwnd, true);
+                    };
 
-                        window.ShowDialog();
 
-                        var resultField = typeof(TWindow).GetField("Result");
-                        if (resultField != null)
+                    window.ShowDialog();
+
+                    source.Cancel();
+                    PluginContext.Log.Info($"ShowWindow.{typeof(TWindow).Name}.Invoke :: External screen closed.");
+                }
+                catch (Exception ex)
+                {
+                    PluginContext.Log.Error($"ShowWindow.{typeof(TWindow).Name} ::{ex.Message}", ex);
+                }
+
+                return window;
+            });
+            var resultArgs = window?.GetType()?.GetField("Result");
+            if (resultArgs is not null)
+            {
+                result = window.Dispatcher.Invoke(()=>(TResult)resultArgs.GetValue(window));
+                if (result is null)
+                {
+                    PluginContext.Log.Warn(
+                        $"ShowWindow.{typeof(TWindow).Name} :: Result {nameof(TResult)}  is null.");
+                }
+            }
+            else
+            {
+                var resultProperty = window?.GetType()?.GetProperty("Result");
+                if (resultProperty is not null)
+                {
+                    result = window.Dispatcher.Invoke(() => (TResult)resultProperty.GetValue(window));
+                    if (result is null)
+                    {
+                        PluginContext.Log.Warn(
+                            $"ShowWindow.{typeof(TWindow).Name} :: Result {nameof(TResult)} is null.");
+                    }
+                }
+                else
+                {
+                    PluginContext.Log.Warn($"ShowWindow.{typeof(TWindow).Name} :: Doesn't have Result data.");
+                }
+
+            }
+
+            PluginContext.Log.Info($"ShowWindow.{typeof(TWindow).Name} :: External screen closed.");
+            return result;
+        }, source.Token);
+
+        // t.Start();
+        var x = t.ConfigureAwait(false).GetAwaiter().GetResult();
+
+
+        return x;
+    }
+
+
+    public static TResult ShowWindowAsync<TWindow, TRequest, TResult>(TRequest args)
+      where TWindow : System.Windows.Window, new()
+      where TRequest : class
+      where TResult : class
+    {
+        var source = new CancellationTokenSource();
+        TWindow window = null;
+        TResult result = null;
+        PluginContext.Log.Info($"ShowWindowAsync.{typeof(TWindow).Name} :: External screen opened.");
+
+        var t = Task.Run(() =>
+        {
+            window = Application.Current.Dispatcher.Invoke(() =>
+            {
+                try
+                {
+                    PluginContext.Log.Info($"ShowWindowAsync.{typeof(TWindow).Name}.Invoke :: External screen opened.");
+                    window = new TWindow();
+                    var argsMethod = typeof(TWindow).GetMethod("AddExternalProperties");
+                    if (argsMethod is not null)
+                    {
+                        if (args is not null)
                         {
-                            result = resultField.GetValue(window) as TResult;
+                            argsMethod.Invoke(window, new object[] { args });
                         }
                         else
                         {
-                            PluginContext.Log.Warn($"Field Result is missing in {typeof(TWindow).Name}.");
+                            argsMethod.Invoke(window, null);
+                            PluginContext.Log.Warn(
+                                $"ShowWindowAsync.{typeof(TWindow).Name} :: AddExternalProperties {nameof(TRequest)} args is null.");
                         }
-                    });
-                }).Wait();
-            }
-            catch (Exception ex)
+                    }
+                    else
+                    {
+                        PluginContext.Log.Warn(
+                            $"ShowWindowAsync.{typeof(TWindow).Name} :: Doesn't have AddExternalProperties method.");
+                    }
+
+                    window.Show();
+
+                    source.Cancel();
+                    PluginContext.Log.Info($"ShowWindowAsync.{typeof(TWindow).Name}.Invoke :: External screen closed.");
+                }
+                catch (Exception ex)
+                {
+                    PluginContext.Log.Error($"ShowWindowAsync.{typeof(TWindow).Name} ::{ex.Message}", ex);
+                }
+
+                return window;
+            });
+            var resultArgs = window?.GetType()?.GetField("Result");
+            if (resultArgs is not null)
             {
-                PluginContext.Log.Error($"Error while opening window {typeof(TWindow).Name}: {ex.Message}", ex);
+                result = window.Dispatcher.Invoke(() => (TResult)resultArgs.GetValue(window));
+                if (result is null)
+                {
+                    PluginContext.Log.Warn(
+                        $"ShowWindowAsync.{typeof(TWindow).Name} :: Result {nameof(TResult)}  is null.");
+                }
+            }
+            else
+            {
+
+                var resultProperty = window?.GetType()?.GetProperty("Result");
+                if (resultProperty is not null)
+                {
+                    result = window.Dispatcher.Invoke(() => (TResult)resultProperty.GetValue(window));
+                    if (result is null)
+                    {
+                        PluginContext.Log.Warn(
+                            $"ShowWindowAsync.{typeof(TWindow).Name} :: Result {nameof(TResult)} is null.");
+                    }
+                }
+                else
+                {
+                    PluginContext.Log.Warn($"ShowWindowAsync.{typeof(TWindow).Name} :: Doesn't have Result data.");
+                }
             }
 
+            PluginContext.Log.Info($"ShowWindowAsync.{typeof(TWindow).Name} :: External screen closed.");
             return result;
-        }
+        }, source.Token);
 
-        [DllImport("user32.dll")]
-        private static extern bool SetCursorPos(int x, int y);
+        // t.Start();
+        var x = t.ConfigureAwait(false).GetAwaiter().GetResult();
 
-        [DllImport("user32.dll")]
-        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        return x;
+    }
 
-        /// <summary>
-        /// Simulating a Left Mouse Click.
-        /// </summary>
-        public static void LeftMouseClick(int xpos, int ypos)
-        {
-            SetCursorPos(xpos, ypos);
-            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
-        }
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern bool SetCursorPos(int x, int y);
 
-        /// <summary>
-        /// Handling Input with the Ability to Return a Result.
-        /// </summary>
-        public static TResponse HandleInput<TResponse>(this Window window, Func<TResponse> func)
-        {
-            TResponse response = default;
-            if (window == null) return response;
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
-            try
-            {
-                window.WindowState = WindowState.Minimized;
+    private const int MOUSEEVENTF_LEFTDOWN = 0x02;
 
-                response = func();
-            }
-            catch (Exception ex)
-            {
-                PluginContext.Log.Error($"Error while handling input: {ex.Message}", ex);
-            }
-            finally
-            {
-                RestoreWindow(window);
-            }
+    private const int MOUSEEVENTF_LEFTUP = 0x04;
 
+    //This simulates a left mouse click
+    public static void LeftMouseClick(int xpos, int ypos)
+    {
+        SetCursorPos(xpos, ypos);
+        mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+    }
+
+
+    public static TResponse HandleInput<TResponse>(this Window window, Func<TResponse> func)
+    {
+        PluginContext.Log.Info("Helpers.HandleInput.Func :: started");
+        TResponse response = default;
+        if (window is null)
             return response;
-        }
-
-        /// <summary>
-        /// Handling Input Without Returning a Result.
-        /// </summary>
-        public static void HandleInput(this Window window, Action action)
+        try
         {
-            if (window == null) return;
+            window.Topmost = false;
+            //window.Hide();
+            window.WindowState = WindowState.Minimized;
 
-            try
-            {
-                window.WindowState = WindowState.Minimized;
-
-                action();
-            }
-            catch (Exception ex)
-            {
-                PluginContext.Log.Error($"Error while handling input: {ex.Message}", ex);
-            }
-            finally
-            {
-                RestoreWindow(window);
-            }
+            response = func();
         }
-
-        private static void RestoreWindow(Window window)
+        catch (Exception ex)
         {
-            if (window == null) return;
-
+            PluginContext.Log.Error($"Helpers.HandleInput.Func :: {ex.Message}", ex);
+        }
+        finally
+        {
             window.WindowState = WindowState.Normal;
             window.Topmost = true;
             window.Focusable = true;
             Keyboard.Focus(window);
+
+
+            //window.Focus();
+            //window.Activate();
+            //window.BringIntoView();
+            ////window.ShowActivated = true;
+            //window.ShowDialog();
         }
+
+        PluginContext.Log.Info("Helpers.HandleInput.Func :: finished");
+
+        return response;
+    }
+
+    public static void HandleInput(this Window window, Action action)
+    {
+        PluginContext.Log.Info("Helpers.HandleInput.Action :: started");
+        window.WindowState = WindowState.Minimized;
+        try
+        {
+            action();
+        }
+        catch (Exception ex)
+        {
+            PluginContext.Log.Error($"Helpers.HandleInput.Action :: {ex.Message}", ex);
+        }
+        finally
+        {
+            window.WindowState = WindowState.Normal;
+        }
+
+        PluginContext.Log.Info("Helpers.HandleInput.Action :: finished");
     }
 }
